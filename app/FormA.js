c724fbf50d341fa790d189e5dc7cdf362f642a22
mkumar363 <46664866+mkumar363@users.noreply.github.com>
2024-01-07 21:10:03 +0530

    FormA.js

--- app/FormA.js
+++ app/FormA.js
@@ -2,14 +2,22 @@
 import { useState } from "react";
 
 function FormA({ onSubmit, age }) {
+    const [dcShows,setDcShows]= useState("")
     const handleSubmit = (event) => {
+        event.preventDefault()
+        onSubmit({
+            type:"Form A",
+            age:age,
+            dcShows:dcShows,
+            marvelShows:null
+        });
     };
     return (
         <form id="dc" onSubmit={handleSubmit}>
             <h2>Form A</h2>
             <label>
                 Select DC Shows:
-                <select  >
+                <select onChange={(e)=>setDcShows(e.target.value)} >
                     <option value="">--Select--</option>
                     <option value="The Flash">The Flash</option>
                     <option value="Arrow">Arrow</option>
